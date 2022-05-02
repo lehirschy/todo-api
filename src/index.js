aa3c05e1ebd6642eef6e67b6b8fe5a0ca40049e0
lehirschy <edensjunk@yahoo.com>
2022-05-02 18:06:19 -0500

    Error Handling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import env from './utils/config'
 import logger from './utils/logger'
 import cors from 'cors'
 import { config } from 'dotenv'
+import errors from './utils/errors'
 
 const app = express()
 
@@ -21,5 +22,6 @@ app.get('/', (req, res) => {
     logger.log.success('Calling Root')
     res.send({msg: 'Hello There!'})
 })
-
+app.use(errors.notFound)
+app.use(errors.errorHandler)
 app.listen(port)
---
+++ src/utils/errors.js
@@ -0,0 +1,21 @@
+import logger from './logger'
+
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.url}`)
+  res.status(404)
+  next(error)
+}
+
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+
+  logger.log.error(new Error(error.message))
+
+  res.status(statusCode)
+  res.send({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '💩' : error.stack,
+  })
+}
+
+export default { notFound, errorHandler }
